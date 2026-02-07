5fde8a3d716f9d534bdffdaf14ae0c34d8d702ed
Raquel Miller <mil22025@byui.edu>
2026-02-07 00:04:27 -0700

    Imported the modules

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
-// Application's entry point
+//Import the modules to use them in the menu
+import { Habit } from "./habits/Habit.js";
+import { Pomodoro } from "./pomodoro/PomodoroTimer.js";
+
 console.log("Pomodoro-Habits app starting ...");
+
+
+//Menu System
+
 
 // console.log("Countdown finished!");
 // console.log(`Time left: ${this._counter}s`);
+
+//Send a message if user wants to start the timer again after changing duration
