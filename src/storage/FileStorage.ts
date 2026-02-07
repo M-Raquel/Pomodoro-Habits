53d5b31f76f7e200e33eb880938cf7af3d4efcb7
Raquel Miller <mil22025@byui.edu>
2026-02-07 00:04:59 -0700

    Basic outline of the FileStorage class

--- src/storage/FileStorage.ts
+++ src/storage/FileStorage.ts
@@ -1,1 +1,20 @@
-// Reads/Writes to JSON files
+import { Habit} from "../habits/Habit.js";
+// Class that Reads/Writes to JSON files.
+
+class FileStorage{
+    //Only needs 1 field, a file path
+    private _filepath: string;
+
+    constructor(filepath: string){
+        this._filepath = filepath;
+    }
+
+    //Only needs two methods, to convert habit objects into JSON and laod them.
+    public saveHabits(habits: Habit[]){
+
+    }
+
+    public loadHabits(){
+        //Reads the JSON file 
+    }
+}
